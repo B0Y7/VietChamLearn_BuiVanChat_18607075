45eecae8745cefb14316cf3b78d2f7a06d496894
B0Y7 <buichat1122@gmail.com>
2022-02-16 22:10:09 +0700

    Add project files.

---
+++ VietChamLearn_BuiVanChat_18607075/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace VietChamLearn_BuiVanChat_18607075
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ VietChamLearn_BuiVanChat_18607075/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VietChamLearn_BuiVanChat_18607075.MvcApplication" Language="C#" %>
